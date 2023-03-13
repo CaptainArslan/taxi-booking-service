34c48f58323af31a980fb3ba9d5c2cedbd567ab3
CaptainArslan <87141902+CaptainArslan@users.noreply.github.com>
2023-03-13 20:14:00 +0500

    update pages view

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+Route::get('/index', function () {
+    return view('index');
+});
+Route::get('/about-us', function () {
+    return view('about-us');
+});
+Route::get('/contact', function () {
+    return view('contact');
 });
