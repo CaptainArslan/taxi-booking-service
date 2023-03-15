6184779af3f9d1f3ed6258ee08984d78b3dd74a8
Syedqasim2021 <sqasimalishah2021@gmail.com>
2023-03-16 00:37:47 +0500

    added the cta-section blade and included it

--- resources/views/about-us.blade.php
+++ resources/views/about-us.blade.php
@@ -25,20 +25,7 @@
 
 @include('partials.team-section')
 
-<section class="cta-section padding">
-    <div class="cta-men"></div>
-    <div class="container">
-        <div class="cta-content">
-            <h2>Call Us Now <span>Book Your Taxi</span> <br> For Your Next Ride!</h2>
-            <p>We successfully cope with tasks of varying complexity,<br>guarantees and regularly master new technologies.</p>
-            <div class="cta-call">
-                <i class="las la-phone-volume"></i>
-                <p><span>Call For Taxi</span><a href="tel:5267214392">5267-214-392</a></p>
-            </div>
-        </div>
-    </div>
-</section>
-<!--/.cta-section-->
+@include('partials.cta-section')
 
 @include('partials.blog-section')
 
---
+++ resources/views/partials/cta-section.blade.php
@@ -0,0 +1,22 @@
+<section class="cta-section padding">
+    <div class="cta-men wow fade-in-bottom" data-wow-delay="200ms"></div>
+    <div class="container">
+        <div class="cta-content">
+            <h2>Call Us Now <span>Book Your Taxi</span> <br> For Your Next Ride!</h2>
+            <p>We successfully cope with tasks of varying complexity,<br>guarantees and regularly master new
+                technologies.</p>
+            <div class="cta-call">
+                <i class="las la-phone-volume"></i>
+                <p><span>Call For Taxi</span><a href="tel:416-260-0050">416-260-0050</a></p>
+            </div>
+            <div class="cta-content">
+                <h2 class="mt-2">OR</h2>
+            </div>
+            <div class="cta-call">
+                <i class="las la-envelope-open"></i>
+                <p><span>Email at</span><a href="mailto:info@blacklimo.ca">info@blacklimo.ca</a></p>
+            </div>
+        </div>
+    </div>
+</section>
+<!--/.cta-section-->
